2a38bf65e86404ee82da0e773f79f063c8984e30
Brent <jbmsharpe@gmail.com>
2023-01-13 15:02:47 -0500

    added scrolling animation so divs slide in from the left side when scrolling

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const navBarLinks = document.getElementById('navbar-links');
 hamburgerButton.addEventListener('click', () => {
     navBarLinks.classList.toggle('active')
 })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElemtns = document.querySelectorAll('.hidden');
+hiddenElemtns.forEach((el) => observer.observe(el));
