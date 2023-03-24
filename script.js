500cb205678d644e3991cbd2a2225a9c10faad17
Brent <jbmsharpe@gmail.com>
2023-03-24 13:45:31 -0400

    took off pricing plans

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-// const hamburgerButton = document.getElementById('hamburger-button');
-// const navBarLinks = document.getElementById('navbar-links');
-
-// hamburgerButton.addEventListener('click', () => {
-//     navBarLinks.classList.toggle('active')
-// })
 
 
 
